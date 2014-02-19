a6d4399356984a6475021a6e8c32c01356267eb4
Luis Ruiz <luis_ruiz_pavon@hotmail.com>
2014-02-19 14:31:00 +0100

    Initial commit

---
+++ tests/YoseTheGame.Tests/StartTests.cs
@@ -0,0 +1,10 @@
+﻿namespace YoseTheGame.Tests
+{
+    public class StartTests
+    {
+        public void Your_page_should_include_the_text_Hello_Yose()
+        {
+            
+        }
+    }
+}
